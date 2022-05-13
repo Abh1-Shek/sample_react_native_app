5a84a189cc3ee0d39daf70f45c8b4d7bdd5b233d
Abh1-Shek <2019uee0141@iitjammu.ac.in>
2022-05-13 05:36:54 +0530

    put a picture in middle of the screen

---
+++ constants/Colors.js
@@ -0,0 +1,4 @@
+export const Colors = {
+    yellow: '#fdbb21',
+    grey: '#898989',
+};
--- screens/Screen1.js
+++ screens/Screen1.js
@@ -1,14 +1,32 @@
-import { StyleSheet } from 'react-native'
+import { StyleSheet, View, Image, Text } from 'react-native';
+import { Colors } from '../constants/Colors';
+
+
 
 function Screen1() {
-    
+    return (
+        <View style={styles.outterContainer}>
+            <Image 
+                style={styles.square}
+                source={require('../assets/shinchan.jpg')}></Image>
+            <Text>Shinchan</Text>
+            <Text>My favourite show</Text>
+        </View>
+    );
 }
 
 export default Screen1;
 
 
 const styles = StyleSheet.create({
-
+    square: {
+        height: 200,
+        width: 200,
+        borderRadius: 100,
+    },
+    outterContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 });
-
-
